b1e3ea37fe40e50721a9cbc62c2772ec797ef83b
Dmytro Iovenko <iovenko@gmail.com>
2024-09-14 01:37:48 -0400

    add user custom validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,15 @@ import User from "../models/user.js";
 // Asynchronous function to create a new user
 const createUser = async (req, res) => {
   try {
-    const newUser= await User.create(req.body);
+    const newUser = await User.create(req.body);
     res.send(newUser).status(201);
   } catch (err) {
+    // Set custom error for unique keys
+    // https://stackoverflow.com/a/66511297
+    if (err.code === 11000 || err.code === 11001) {
+      err.message = `The ${Object.keys(err.keyValue)[0]} already exists`;
+      console.log(err);
+    }
     res.send(err).status(400);
   }
 };
--- models/user.js
+++ models/user.js
@@ -1,20 +1,41 @@
 import mongoose from "mongoose";
 
+// Check that name contains at least two unique characters
+const hasNonUniqueChars = (name) => !/^(.)\1+$/i.test(name);
+
+// Check that email is a valid email address
+// https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+const isValidEmail = (email) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(email);
+
+// Check that email is not from the domain "example.com"
+const isNotExampleCom = (email) => !/example\.com$/.test(email);
+
 // Define a schema for 'users' collection
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      trim: true,
+      required: [true, "The name cannot be blank"],
+      minLength: [4, "The name must be at least four characters long"],
+      validate: [hasNonUniqueChars, "The name must contain at least two unique characters"],
     },
     email: {
       type: String,
-      required: true,
-      unique: true,
+      trim: true,
+      required: [true, "The email cannot be blank"],
+      validate: [isValidEmail, "The email must be a valid email address"],
+      validate: [isNotExampleCom, 'The email must not be from the domain "example.com"'],
+      index: {
+        //https://stackoverflow.com/questions/13991604/mongoose-schema-validating-unique-field-case-insensitive
+        unique: true,
+        collation: { locale: "en", strength: 2 },
+      },
     },
   },
   { timestamps: true } // assign createdAt and updatedAt fields
 );
+// unique: [true, "The email already exists"],
 
 // Create an index on the 'email' field to enforce uniqueness
 userSchema.index({ email: 1 });
